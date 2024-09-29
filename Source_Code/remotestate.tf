9b70487ab44998c9aeb792d5fc8aeb7e4076553d
Harshal Pundlik Patil <hpharshalmech@gmail.com>
2024-09-29 19:19:59 +0530

    Update remotestate.tf

--- Source_Code/remotestate.tf
+++ Source_Code/remotestate.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "tfstate-management-bucket"
+    bucket = "ADD_BUCKET_NAME_HERE"
     key    = "terraform.tfstate"
     region = "us-east-1"
-    dynamodb_table = "StateLock"
+    dynamodb_table = "DYNAMO_DB_TABLE_NAME_HERE"
   }
 }
